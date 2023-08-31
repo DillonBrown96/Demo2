a13079d6aadc0a302700a7171cd55120fb3fb2d6
Dillon <s0299646@student.rockvalleycollege.edu>
2023-08-30 19:31:28 -0500

    Initial Commit

---
+++ Demo2/Demo2App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo2App.swift
+//  Demo2
+//
+//  Created by Dillon Brown on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Demo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
