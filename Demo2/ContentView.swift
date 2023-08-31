49d75e1ea029f53848389722f7def483312da38f
Dillon <s0299646@student.rockvalleycollege.edu>
2023-08-30 19:37:08 -0500

    updated greeting

--- Demo2/ContentView.swift
+++ Demo2/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello From Dillon demo 2!")
         }
         .padding()
     }
